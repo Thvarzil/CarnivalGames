d100693a4ce9c1bd157a6152835e4cc6bbd18dba
Thvarzil <louisblebohec@gmail.com>
2021-02-09 17:03:23 -0700

    progress commit - productinfo form complete

--- productinfo.php
+++ productinfo.php
@@ -10,41 +10,46 @@ and open the template in the editor.
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--    Add in Boostrap CSS to help with Navbar, grid placement, and general style consistency    -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <!--    Establish custom favicon for flair    -->
     <link rel="icon" href="./img/favicon.png">
 </head>
 <body>
-<?php
-    $locations = explode("\n", file_get_contents("./text/servicearea.txt"));
-?>
 
-<form method = "post" action = "ResultPage.php">
-    <div class="form-group">
-        <label for="products">Our Products</label>
-        <select id="products" name="ship">
-            <option value="--">--</option>
+<container>
+    <div class="jumbotron">
+        <form method="post" action="response.php">
+            <div class="form-group">
+                <label for="products">Our Products</label>
+                <select id="products" name="products">
+                    <option value="--">--</option>
+                    <option value="Chess">--</option>
+                    <option value="Board Games">Board Games</option>
+                    <option value="Home Escape Rooms">Home Escape Room</option>
+                    <option value="Murder Parties">Murder Parties</option>
+                    <option value="Educational Games">Educational Games</option>
+                </select>
+                <label for="location">Your Location</label>
+                <select id="location" name="location">
+                    <option value="--">--</option>
+                    <?php
+                    $locations = explode("\n", file_get_contents("./text/servicearea.txt"));
+                    for ($i = 0; $i < sizeof($locations); $i++) {
+                        print("<option value=" . $i . ">" . $locations[$i] . "</option>");
+                    }
+                    ?>
+                </select>
+                <lable for="name">Your Name</lable>
+                <input id="name" name="name" type=text>
+                <input type="submit" name="Submit" value="Submit">
 
-        </select >
-        <label for="location">Your Location</label>
-        <select id="location" name="departure">
-            <option value="--">--</option>
-            <?php
-            for($i=0;$i<sizeof($locations);$i++){
-                print("<option value=".$i.">".$locations[$i]."</option>");
-            }
-            ?>
-        </select>
-        <lable for="name">Destination planet</lable>
-        <select id="name" name="arrival">
-            <option value="--">--</option>
+            </div>
 
-        </select>
-        <input type="submit" name="Create" value="Get the Estimate">
-
+        </form>
     </div>
-</form>
+</container>
 <?php
 // put your code here
 ?>
