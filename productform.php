4e1cb0c999491180b802e5585596ce88d4b5a1ce
Thvarzil <louisblebohec@gmail.com>
2021-03-05 14:47:56 -0700

    build form on productform.php

--- productform.php
+++ productform.php
@@ -10,8 +10,33 @@ and open the template in the editor.
     <title></title>
 </head>
 <body>
-<?php
-// put your code here
-?>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1>Product Form</h1>
+    </div>
+
+    <div class="jumbotron">
+        <form method="post" action="results.php" class="form-horizontal">
+            <div class="form-group">
+                <label for="prodname">Product Name:</label>
+                <input name="prodname" type="text">
+                <label for="prodprice">Product Price: $</label>
+                <input name="prodprice" type="number" min="0">
+                <label for="proddesc">Product Description:</label>
+                <textarea name="proddesc" rows="5" cols="50"></textarea>
+                <label for="prodimg">Product Image:</label>
+                <input name="prodimg" type="text">
+                <label for="prodaction">Action:</label>
+                <select name="prodaction">
+                    <option value="Add">Add</option>
+                    <option value="Search">Search</option>
+                    <option value="Update">Update</option>
+                </select>
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
+
+</div>
 </body>
 </html>
