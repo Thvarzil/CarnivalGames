77335aa77a108176f33dc22337f4fd57981d67f6
Thvarzil <louisblebohec@gmail.com>
2021-03-05 15:26:56 -0700

    cleaned up presentation of productform.php

--- productform.php
+++ productform.php
@@ -7,32 +7,62 @@ and open the template in the editor.
 <html>
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>Product Form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    Add in Boostrap CSS to help with Navbar, grid placement, and general style consistency    -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <!--    Establish custom favicon for flair    -->
+    <link rel="icon" href="./img/favicon.png">
 </head>
 <body>
 <div class="container-fluid">
-    <div class="jumbotron">
+    <div class="jumbotron" style="width: 100%; align-content: center">
         <h1>Product Form</h1>
     </div>
 
-    <div class="jumbotron">
-        <form method="post" action="results.php" class="form-horizontal">
+    <div class="jumbotron" style="width: 100%; align-content: center">
+        <form method="post" action="results.php" class="">
             <div class="form-group">
-                <label for="prodname">Product Name:</label>
-                <input name="prodname" type="text">
-                <label for="prodprice">Product Price: $</label>
-                <input name="prodprice" type="number" min="0">
-                <label for="proddesc">Product Description:</label>
-                <textarea name="proddesc" rows="5" cols="50"></textarea>
-                <label for="prodimg">Product Image:</label>
-                <input name="prodimg" type="text">
-                <label for="prodaction">Action:</label>
-                <select name="prodaction">
-                    <option value="Add">Add</option>
-                    <option value="Search">Search</option>
-                    <option value="Update">Update</option>
-                </select>
-                <input type="submit" value="Submit">
+                <table>
+                    <tr>
+                        <td><label for="prodname">Product Name:</label></td>
+                        <td width="10"></td>
+                        <td><input name="prodname" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="prodprice">Product Price: $</label></td>
+                        <td></td>
+                        <td><input name="prodprice" type="number" min="0"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="proddesc">Product Description:</label></td>
+                        <td></td>
+                        <td><textarea name="proddesc" rows="5" cols="50"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td><label for="prodimg">Product Image:</label></td>
+                        <td></td>
+                        <td><input name="prodimg" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="prodaction">Action:</label></td>
+                        <td></td>
+                        <td><select name="prodaction">
+                                <option value="Add">Add</option>
+                                <option value="Search">Search</option>
+                                <option value="Update">Update</option>
+                            </select></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <input type="submit" value="Submit">
+                        </td>
+                    </tr>
+                </table>
             </div>
         </form>
     </div>
