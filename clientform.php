9f995fadb36b70a5fb9eca83e2b1108b06ea7b11
Thvarzil <louisblebohec@gmail.com>
2021-04-27 16:40:19 -0600

    beginning work on newsletter sign up page

--- clientform.php
+++ clientform.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Client Form Page</title>
-    </head>
+<head>
+    <title>Carnival Games - Newsletter Sign Up</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    Add in Boostrap CSS to help with Navbar, grid placement, and general style consistency    -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <!--    Establish custom favicon for flair    -->
+    <link rel="icon" href="./img/favicon.png">
+</head>
     <body>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Carnival Games</a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./productinfo.php">Product Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Newsletter Sign Up</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./productform.php">Product Form</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Order Online</a>
+            </li>
+        </ul>
+
+    </nav>
+    <div class="jumbotron">
         <form id="form_11683" class="appnitro"  method="post" action="completed.php">
             <div class="form_description">
                 <h2>New Client</h2>
@@ -113,6 +144,7 @@
                     <input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
                 </li>
             </ul>
-        </form>	
+        </form>
+    </div>
     </body>
 </html>
