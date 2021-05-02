b50d228653fb6f64277b557083e76b85b9a6f68c
Thvarzil <louisblebohec@gmail.com>
2021-05-02 14:50:26 -0600

    Begin work on login functionality

--- login.php
+++ login.php
@@ -5,13 +5,43 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
+<head>
+    <title>Carnival Games</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    Add in Boostrap CSS to help with Navbar, grid placement, and general style consistency    -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <!--    Establish custom favicon for flair    -->
+    <link rel="icon" href="./img/favicon.png">
+</head>
     <body>
         <?php
         // put your code here
         ?>
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">Carnival Games</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="productinfo.php">Product Info</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="clientform.php">Newsletter Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="productform.php">Product Form</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Order Online</a>
+                </li>
+            </ul>
+
+        </nav>
     </body>
 </html>
